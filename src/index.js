7712da6e1eb971ba1d19637600ab8e14855df21d
Anna Larus <77969817+anlarus@users.noreply.github.com>
2021-08-07 04:21:22 -0300

    feat:add toReadable function tested

--- src/index.js
+++ src/index.js
@@ -1,3 +1,143 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    let group1 = [
+        "zero",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+        "twenty",
+    ];
+
+    let group2 = [
+        "",
+        "ten",
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+
+    let hundred = "hundred";
+    let userOutput, firstWord, secondWord, thirdWord;
+
+    if (!isNaN(+number) && +number >= 0 && +number <= 999) {
+        if (number <= 20) {
+            group1.forEach((item) => {
+                number === group1.indexOf(item)
+                    ? (userOutput = item)
+                    : userOutput;
+            });
+        } else if (+number > 20 && +number < 100) {
+            if (+number % 10 === 0) {
+                group2.forEach((item) => {
+                    +number / 10 === group2.indexOf(item)
+                        ? (firstWord = item)
+                        : firstWord;
+                });
+                userOutput = `${firstWord}`;
+            } else {
+                group2.forEach((item) => {
+                    Math.floor(+number / 10) === group2.indexOf(item)
+                        ? (firstWord = item)
+                        : firstWord;
+                });
+                group1.forEach((item) => {
+                    +number % 10 === group1.indexOf(item)
+                        ? (secondWord = item)
+                        : secondWord;
+                });
+                userOutput = `${firstWord} ${secondWord}`;
+            }
+        } else if (+number % 10 === 0) {
+            if (+number % 100 === 0) {
+                group1.forEach((item) => {
+                    +number / 100 === group1.indexOf(item)
+                        ? (firstWord = item)
+                        : firstWord;
+                });
+                userOutput = `${firstWord} ${hundred}`;
+            } else {
+                group1.forEach((item) => {
+                    Math.floor(+number / 100) === group1.indexOf(item)
+                        ? (firstWord = item)
+                        : firstWord;
+                });
+                group2.forEach((item) => {
+                    Math.floor((+number % 100) / 10) === group2.indexOf(item)
+                        ? (secondWord = item)
+                        : secondWord;
+                });
+                userOutput = `${firstWord} ${hundred} ${secondWord}`;
+            }
+        } else {
+            if (Math.floor((+number / 10) % 10) === 0) {
+                group1.forEach((item) => {
+                    Math.floor(+number / 100) === group1.indexOf(item)
+                        ? (firstWord = item)
+                        : firstWord;
+                });
+                group1.forEach((item) => {
+                    Math.floor(Math.floor(+number % 10) % 10) ===
+                    group1.indexOf(item)
+                        ? (thirdWord = item)
+                        : thirdWord;
+                });
+                userOutput = `${firstWord} ${hundred} ${thirdWord}`;
+            } else {
+                if (+number % 100 < 20) {
+                    group1.forEach((item) => {
+                        Math.floor(+number / 100) === group1.indexOf(item)
+                            ? (firstWord = item)
+                            : firstWord;
+                    });
+                    group1.forEach((item) => {
+                        +number % 100 === group1.indexOf(item)
+                            ? (secondWord = item)
+                            : secondWord;
+                    });
+                    userOutput = `${firstWord} ${hundred} ${secondWord}`;
+                } else {
+                    group1.forEach((item) => {
+                        Math.floor(+number / 100) === group1.indexOf(item)
+                            ? (firstWord = item)
+                            : firstWord;
+                    });
+                    group2.forEach((item) => {
+                        Math.floor((+number % 100) / 10) ===
+                        group2.indexOf(item)
+                            ? (secondWord = item)
+                            : secondWord;
+                    });
+                    group1.forEach((item) => {
+                        Math.floor(Math.floor(+number % 10) % 10) ===
+                        group1.indexOf(item)
+                            ? (thirdWord = item)
+                            : thirdWord;
+                    });
+
+                    userOutput = `${firstWord} ${hundred} ${secondWord} ${thirdWord}`;
+                }
+            }
+        }
+    }
+    return userOutput;
+};
